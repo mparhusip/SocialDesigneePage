580eb2c381b2428dcffaea6b8c15e21384d35021
Marilyn Martha Yusnita Devi Parhusip <marilyn.parhusip@gmail.com>
2019-08-27 20:21:22 +0700

    Initial Commit

---
+++ SocialDesigneePage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialDesigneePage
+//
+//  Created by Marilyn Martha Yusnita Devi Parhusip on 27/08/19.
+//  Copyright © 2019 Apple Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
